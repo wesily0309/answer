bfd09d03ccaf4933aba8381110daa397a90da108
阿昊 <110410452+wesily0309@users.noreply.github.com>
2023-11-03 09:26:42 +0800

    Update eee.py

--- eee.py
+++ eee.py
@@ -3,15 +3,17 @@ b=[]
 for i in range(a):
     e=input('xxx(銷貨填負) xxx:').split(' ')
     b.append(list(map(int,e)))
-#[數量,價格]
 c=0
 r=0
 for i in range(len(b)):
     if b[i][0]<0:
         f=0
         for d in range(abs(b[i][0])):
-            if b[f][0]<=0:
-                f+=1
+            for _ in range(len(b)):
+                if b[f][0]<=0:
+                    f+=1
+                else:
+                    break  
             c+=b[f][1]
             r+=b[i][1]
             b[f][0]-=1
